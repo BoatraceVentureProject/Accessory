578685fb3568fa52fdf21059ef91ebef2dacf26e
shimomo <developer@shimomo.net>
2025-01-16 23:51:43 +0900

    Rename method from testComments01() to testCommentsForRaceNumber1AndDate20240103() in Stadium22Test to make the test name more descriptive

--- tests/Stadiums/Stadium22Test.php
+++ tests/Stadiums/Stadium22Test.php
@@ -107,7 +107,7 @@ class Stadium22Test extends PHPUnitTestCase
     /**
      * @return void
      */
-    public function testComments01(): void
+    public function testCommentsForRaceNumber1AndDate20240103(): void
     {
         $response = $this->stadium->comments(raceNumber: 1, date: '2024-01-03');
         $this->assertSame('益田啓司', $response['bracket_1_racer_name']);
